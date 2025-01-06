40b96707adef2df54f0230dc028f597bdb8baa02
corwsan <191419896+corwsan@users.noreply.github.com>
2025-01-06 20:17:34 +1300

    Add hover effect to change color of divs on mouse over

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 const container = document.querySelector("#container");
 const squareDiv = document.createElement("div");
+squareDiv.classList.add("square");
+
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("square")) {
+        event.target.classList.add("red");
+    }
+})
 
 for (let i = 0; i < 16; i++) {
     container.appendChild(squareDiv.cloneNode(true))
 }
+
