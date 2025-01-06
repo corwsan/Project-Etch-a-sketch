b73f09006f47fbfd558f9db38e2ee9a8d7eeb1b3
corwsan <191419896+corwsan@users.noreply.github.com>
2025-01-06 20:42:21 +1300

    Add button to create new custom size grid

--- javascript.js
+++ javascript.js
@@ -1,6 +1,26 @@
 const container = document.querySelector("#container");
+
 const squareDiv = document.createElement("div");
 squareDiv.classList.add("square");
+
+const btnContainer = document.querySelector("#buttons");
+const newGridButton = document.createElement("button");
+newGridButton.textContent = "New Grid";
+btnContainer.appendChild(newGridButton);
+
+newGridButton.addEventListener ("click" , () => {
+    const squares = prompt ("How many squares per side for new grid?");
+    container.innerHTML = "";
+
+    squareDiv.style.width = (960 / squares) - 2+"px";
+    squareDiv.style.height = (960 / squares) - 2+"px";
+
+
+    for (let i = 0; i < (squares * squares); i++) {
+        container.appendChild(squareDiv.cloneNode(true))
+    }
+
+})
 
 container.addEventListener("mouseover", (event) => {
     if (event.target.classList.contains("square")) {
@@ -8,7 +28,5 @@ container.addEventListener("mouseover", (event) => {
     }
 })
 
-for (let i = 0; i < 16; i++) {
-    container.appendChild(squareDiv.cloneNode(true))
-}
+
 
